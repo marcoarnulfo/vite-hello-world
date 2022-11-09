26ec9de6d363ced601e4482d98674fc13c8aca07
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-11-09 14:40:32 +0100

    end App with bonus

---
+++ bonus/my-vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue';
+import AppJumbo from './components/AppJumbo.vue';
+</script>
+
+<template>
+  <AppJumbo/>
+  <h1>Hello</h1>
+  <AppTitle/>
+</template>
+
+<style>
+</style>
---
+++ my-vue-app/src/components/AppJumbo.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+    <div class="mt-4 p-5 bg-primary text-white rounded">
+        <h2>Jumbotron Example</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, laudantium labore ducimus corporis fugit nisi
+            quisquam consectetur illo blanditiis. Ab impedit odit quis esse necessitatibus recusandae libero, aliquam
+            odio tempore.</p>
+    </div>
+</template>
+
+<style>
+
+</style>
